c44dfe9062c77803bd97b8c35fe3a62581c069b6
Theresa Ma <theresa.ma@mail.utoronto.ca>
2014-01-25 05:51:16 -0500

    removed comment filtering

--- content.js
+++ content.js
@@ -21,25 +21,24 @@
 //    	lastInput = document.activeElement.value
 // }
 
-window.addEventListener('keypress', function (e) {
-	if (e.keyCode == 13 && document.activeElement.name == "add_comment_text_text") {
-		e.preventDefault
-		element = document.activeElement.value
-		if (isOffensive(element)){
-    		var r = window.confirm("Are you sure you want to post this?");
-    	}
-    	if( !r )
-    	event.preventDefault();
-	}
-}
+// window.addEventListener('keypress', function (e) {
+// 	if (e.keyCode == 13 && document.activeElement.name == "add_comment_text_text") {
+// 		e.preventDefault
+// 		element = document.activeElement.value
+// 		if (isOffensive(element)){
+//     		var r = window.confirm("Are you sure you want to post this?");
+//     	}
+//     	if( !r )
+//     	event.preventDefault();
+// 	}
+// }
 
 
 var clickedPost = function(event)
 {
 	console.log(event);
 	//event.preventDefault();
-	element = document.querySelectorAll('input[name=xhpc_message]')[0].value;
-	if (isOffensive(element)){
+	if (isOffensive()){
     var r = window.confirm("Are you sure you want to post this?");
     if( !r )
     	event.preventDefault();
@@ -47,17 +46,22 @@ var clickedPost = function(event)
 }
 
 setTimeout(function() {
+	//from the home page
 	element = document.querySelectorAll('button[value="1"][type="submit"]')[0]
 	parent = element.parentNode
 	element.addEventListener('click', clickedPost)
 
+	//from profile page
 	element2 = document.querySelectorAll('button[value="1"][type="submit"]')[2]
 	parent2 = element2.parentNode
 	element2.addEventListener('click', clickedPost)
 
 }, 500);
 
-var isOffensive = function(profanity){
+var isOffensive = function(){
+
+	element = document.querySelectorAll('input[name=xhpc_message]')[0].value;
+
 
 	var bad = new Array();
 	bad[0] = /\w*fuck\w*/
@@ -72,7 +76,7 @@ var isOffensive = function(profanity){
  	bad[9] = /\w*cunt\w*/
 
  	for (var i = bad.length - 1; i >= 0; i--) {
- 		if (bad[i].test(profanity))
+ 		if (bad[i].test(element))
  			return true;
  	};
  	return false;
