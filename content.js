126d69e501f8417e3090361a0cbd40cc1152986a
Theresa Ma <theresa.ma@mail.utoronto.ca>
2014-01-25 09:44:22 -0500

    status posting filtering

--- content.js
+++ content.js
@@ -1,57 +1,28 @@
-// var pressed = false
-// var lastInput = ""
-
-// window.onkeydown = function(e) {   
-// 	var e = window.event||e;
-//    	var key = e.keyCode ? e.keyCode : e.which;
-
-//    	console.log(pressed +", " + key + ", " + document.activeElement.value + ", " + lastInput)
-//    	if (key == 13 && document.activeElement.name == "add_comment_text_text" && !pressed && document.activeElement.value.trim()=="" && lastInput.trim()!="") {
-//    		//alert("serdtfygvbhj")
-//    		pressed = true
-//    		e.preventDefault()
-//        	if(isOffensive(lastInput)){
-//        		    var r = window.confirm("Are you sure you want to comment this?");
-//     			if( !r ){ac
-//     				e.preventDefault()
-//     				pressed = false
-//     			}
-//        	}pressed=false
-//    	}
-//    	lastInput = document.activeElement.value
-// }
-
-
 setInterval(function(){
-  if (document.activeElement.name == "add_comment_text_text"){
+	if (document.activeElement.name == "add_comment_text_text"){
 
-  	document.activeElement.addEventListener('keydown', commentFunction);
+		document.activeElement.addEventListener('keydown', commentFunction);
 	}
 }, 500);
 
 var commentFunction = function(e){
-		var e = window.event||e;
-   		var key = e.keyCode ? e.keyCode : e.which;
-   		
-   		if (key === 13 && isOffensive(document.activeElement.value) && !window.confirm("Are you sure you want to comment this?")){
-   				e.preventDefault()
-   				e.stopPropagation();
-   				return false;
-   			}
+	var e = window.event||e;
+	var key = e.keyCode ? e.keyCode : e.which;
+
+	if (key === 13 && isOffensive(document.activeElement.value) && !window.confirm("Are you sure you want to comment this?")){
+		e.preventDefault()
+		e.stopPropagation();
+		return false;
+	}
 }
 
-
-
-
-var clickedPost = function(event)
+var clickedPost = function()
 {
 	console.log(event);
-	var element = a
-	//event.preventDefault();
-	if (isOffensive(element)){
-    var r = window.confirm("Are you sure you want to post this?");
-    if( !r )
-    	event.preventDefault();
+	if (isOffensive(document.querySelectorAll('input[name="xhpc_message"]')[0].value)){
+		var r = window.confirm("Are you sure you want to post this?");
+		if( !r )
+			event.preventDefault();
 	}
 }
 
@@ -66,30 +37,27 @@ setTimeout(function() {
 	parent2 = element2.parentNode
 	element2.addEventListener('click', clickedPost)
 
-
-
-
 }, 500);
 
 var isOffensive = function(comment){
 
 	var bad = new Array();
 	bad[0] = /\w*fuck\w*/
- 	bad[1] = /\w*fck\w*/
- 	bad[2] = /\w*fuk\w*/
- 	bad[3] = /\w*shit\w*/
- 	bad[4] = /\w*penis\w*/
- 	bad[5] = /\w*fag\w*/
- 	bad[6] = /\w*ass\w*/
- 	bad[7] = /\w*nigger\w*/
- 	bad[8] = /\w*dick\w*/
- 	bad[9] = /\w*cunt\w*/
+	bad[1] = /\w*fck\w*/
+	bad[2] = /\w*fuk\w*/
+	bad[3] = /\w*shit\w*/
+	bad[4] = /\w*penis\w*/
+	bad[5] = /\w*fag\w*/
+	bad[6] = /\w*ass\w*/
+	bad[7] = /\w*nigger\w*/
+	bad[8] = /\w*dick\w*/
+	bad[9] = /\w*cunt\w*/
 
- 	for (var i = bad.length - 1; i >= 0; i--) {
- 		if (bad[i].test(comment))
- 			return true;
- 	};
- 	return false;
+	for (var i = bad.length - 1; i >= 0; i--) {
+		if (bad[i].test(comment))
+			return true;
+	};
+	return false;
 }
 
 
